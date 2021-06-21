c1c4bc27740c68c6fd21d56498214e8eeb0ddec5
David Chin <david.w.h.chin@gmail.com>
2021-06-21 02:28:30 -0400

    Add main()

--- index_all_files_by_checksum.py
+++ index_all_files_by_checksum.py
@@ -5,7 +5,8 @@ from pathlib import Path
 import hashlib
 import pickle
 
-# index_all_files_by_checksum.py - walks the LrC Masters directory and calculates hash for all files (photos)
+# index_all_files_by_checksum.py - walks the LrC Masters directory and
+#     calculates hash for all files (photos)
 # Copyright (C) 2021  David Chin
 #
 # This program is free software: you can redistribute it and/or modify
@@ -23,34 +24,41 @@ import pickle
 
 debug_p = False
 
-home_dir = Path(os.getenv('HOME'))
-base_dir = home_dir / '/Pictures/Masters/'
 
-ds_store = '.DS_Store'
+def main():
+    global debug_p
 
-masters_dir = Path(home_dir) / 'Pictures' / 'Masters'
-hash_index = {}
-for root, dirs, files in os.walk(masters_dir):
-    if debug_p:
-        print('root', root)
-        print('dirs', dirs)
-        print('files', files)
-        print('')
+    # Lightroom Classic's masters folder
+    home_dir = Path(os.getenv('HOME'))
+    masters_dir = Path(home_dir) / 'Pictures' / 'Masters'
 
-    if files:
-        for f in files:
-            file_path = Path(root) / f
-            with open(file_path, 'rb') as photo:
-                filehash = hashlib.blake2b()
-                filehash.update(photo.read())
-                if filehash.hexdigest() not in hash_index:
-                    hash_index[filehash.hexdigest()] = [Path(file_path)]
-                else:
-                    hash_index[filehash.hexdigest()].append(Path(file_path))
+    hash_index = {}
+    for root, dirs, files in os.walk(masters_dir):
+        if debug_p:
+            print('root', root)
+            print('dirs', dirs)
+            print('files', files)
+            print('')
 
-for k, v in hash_index.items():
-    if len(v) > 1:
-        print('DUPE: ', k, v)
+        if files:
+            for f in files:
+                if f != '.DS_Store':
+                    file_path = Path(root) / f
+                    with open(file_path, 'rb') as photo:
+                        filehash = hashlib.blake2b()
+                        filehash.update(photo.read())
+                        if filehash.hexdigest() not in hash_index:
+                            hash_index[filehash.hexdigest()] = [Path(file_path)]
+                        else:
+                            hash_index[filehash.hexdigest()].append(Path(file_path))
 
-with open('all_photo_hashes.pickle', 'wb') as f:
-    pickle.dump(hash_index, f)
+    for k, v in hash_index.items():
+        if len(v) > 1:
+            print('DUPE: ', k, v)
+
+    with open('all_photo_hashes.pickle', 'wb') as f:
+        pickle.dump(hash_index, f)
+
+
+if __name__ == '__main__':
+    main()
